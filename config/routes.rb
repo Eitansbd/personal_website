d0953e18b5466f910f98abf7621149fd9166e238
Eitan Bar-David <eitansbd@gmail.com>
2019-11-11 19:32:07 +0000

    add blog post resource

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   
   get '/projects', to: 'static_pages#projects'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  resources :blog_posts
 end
