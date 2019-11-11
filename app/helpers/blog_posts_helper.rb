0ae29d7b2047582b6cbef99c1f6276fbdbd7f998
Eitan Bar-David <eitansbd@gmail.com>
2019-11-11 21:05:58 +0000

    add redcarpet helper

--- app/helpers/blog_posts_helper.rb
+++ app/helpers/blog_posts_helper.rb
@@ -1,2 +1,22 @@
+require 'rouge'
+
 module BlogPostsHelper
+  # class CustomRender < Redcarpet::Render::HTML
+  #   include Rouge::Plugins::Redcarpet
+  # end
+  
+  def render_markdown(text)
+    extras = {
+       autolink: true,
+       no_intra_emphasis: true,
+       disable_indented_code_blocks: true,
+       fenced_code_blocks: true,
+       strikethrough: true,
+       superscript: true,
+       lax_spacing: true
+     }
+    markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML)
+    #markdown = Redcarpet::Markdown.new(CustomRender, extras)
+    markdown.render(text).html_safe
+  end
 end
