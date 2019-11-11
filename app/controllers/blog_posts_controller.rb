2d11dde89f817a14acb8ef6a363079acb92fcaf2
Eitan Bar-David <eitansbd@gmail.com>
2019-11-11 20:13:09 +0000

    add new, create, show

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -1,2 +1,28 @@
 class BlogPostsController < ApplicationController
+  
+  
+  def new
+    @blog_post = BlogPost.new
+  end
+  
+  def create
+    @blog_post = BlogPost.new(blog_post_params)
+    @blog_post.save_content_to_aws
+    
+    @blog_post.save
+    
+    redirect_to root_url
+  end
+  
+  def show
+    @blog_post = BlogPost.find(params[:id])
+    
+    @blog_post.fetch_content_from_aws
+  end
+  
+  private
+    
+    def blog_post_params
+      params.require(:blog_post).permit(:title, :subtitle, :content)
+    end
 end
