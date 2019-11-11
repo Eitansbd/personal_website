73a435aca917800671fb3e40114d27350d13710e
Eitan Bar-David <eitansbd@gmail.com>
2019-11-11 20:13:34 +0000

    add method for saving and retriving from AWS S3

--- app/models/blog_post.rb
+++ app/models/blog_post.rb
@@ -5,5 +5,23 @@ class BlogPost < ApplicationRecord
   validates :subtitle, presence: true
   validates :aws_obj_key, presence: true
   
+  def save_content_to_aws
+    s3 = Aws::S3::Resource.new
+    bucket = s3.bucket(ENV.fetch("PERSONAL_WEBSITE_BUCKET_NAME"))
+    
+    self.aws_obj_key = 'blog/' + self.title.gsub(" ", "-").downcase + '.md'
+    
+    object = bucket.object(self.aws_obj_key)
+    
+    object.put(body: self.content)
+  end
   
+  def fetch_content_from_aws
+    s3 = Aws::S3::Resource.new
+    bucket = s3.bucket(ENV.fetch("PERSONAL_WEBSITE_BUCKET_NAME"))
+    
+    object = bucket.object(self.aws_obj_key)
+    
+    self.content = object.get.body.string
+  end
 end
