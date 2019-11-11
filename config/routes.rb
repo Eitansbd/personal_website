e84733969b449ca54728fc5a8bb4e74b2aeeb705
Eitan Bar-David <eitansbd@gmail.com>
2019-11-11 14:48:02 +0000

    fix projects route

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   
   root 'static_pages#home'
   
-  get '/projects', to: 'static_pages#home'
+  get '/projects', to: 'static_pages#projects'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
