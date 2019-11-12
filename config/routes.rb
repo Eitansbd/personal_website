04c90271e4f4116aaa1cf6f00f9d3a61b8024ba0
Eitan Bar-David <eitansbd@gmail.com>
2019-11-12 00:31:16 +0000

    add session routes

--- config/routes.rb
+++ config/routes.rb
@@ -5,4 +5,8 @@ Rails.application.routes.draw do
   get '/projects', to: 'static_pages#projects'
   
   resources :blog_posts
+  
+  get '/admin_login', to: 'sessions#new'
+  post '/admin_login', to: 'sessions#create'
+  delete '/admin_logout', to: 'sessions#delete'
 end
