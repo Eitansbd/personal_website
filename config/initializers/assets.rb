8f7c58248ccdb5188cefa92ecc7d8999fdc0dfb7
Eitan Bar-David <eitansbd@gmail.com>
2019-11-12 15:31:58 +0000

    remove 'assets' from image tag path

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -11,4 +11,4 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in the app/assets
 # folder are already added.
-Rails.application.config.assets.precompile += %w( images/* videos/scheduler-demo.mp4 files/* )
+Rails.application.config.assets.precompile += %w( images/* videos/* files/* )
