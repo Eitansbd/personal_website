b830fcdf8129577525b789b8a46c62f0a8c22912
Eitan Bar-David <eitansbd@gmail.com>
2019-11-12 00:50:37 +0000

    create session after successful login

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,19 @@
 class SessionsController < ApplicationController
   def new
-    
+  end
+  
+  def create
+    if params[:admin_password] == ENV['ADMIN_PASSWORD']
+      session[:admin] = 1
+      flash[:success] = "Logged in as Admin"
+      
+      redirect_to blog_posts_url
+    else
+      flash.now[:error] = "Incorrect Password. If you are not the admin, don't try logging in"
+    end
+  end
+  
+  def destroy
+    session.delete(:admin) 
   end
 end
