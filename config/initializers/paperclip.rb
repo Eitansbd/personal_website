997ad4893a29793e11b77f8b0f4f6acf88f5cffa
Eitan Bar-David <eitansbd@gmail.com>
2019-11-13 15:48:09 +0000

    bug gix paperclip configs

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,9 +1,11 @@
-Rails.application.config.papeclip_defaults = {
-  storage: :s3,
-  # s3_credentials: {
-  #   access_key_id: ENV.fetch('PERSONAL_WEBSITE_ACCESS_KEY_ID'),
-  #   secret_access_key: ENV.fetch('PERSONAL_WEBSITE_SECRET_ACCESS_KEY'),
-  #   s3_region: ENV.fetch('PERSONAL_WEBSITE_S3_REGION'),
-  # },
-  # bucket: ENV.fetch('PERSONAL_WEBSITE_BUCKET_NAME')
-}
+Paperclip::Attachment.default_options.update({
+    storage: :s3,
+    s3_protocol: 'https',
+    s3_host_name: 's3-us-east-2.amazonaws.com',
+    s3_credentials: {
+        s3_region: ENV.fetch('PERSONAL_WEBSITE_S3_REGION'),
+        bucket: ENV.fetch('PERSONAL_WEBSITE_BUCKET_NAME'),
+        access_key_id: ENV.fetch('PERSONAL_WEBSITE_ACCESS_KEY_ID'),
+        secret_access_key: ENV.fetch('PERSONAL_WEBSITE_SECRET_ACCESS_KEY')
+    }
+})
