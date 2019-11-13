765ec7b10bf348be1804507d3beafb7329255083
Eitan Bar-David <eitansbd@gmail.com>
2019-11-13 13:22:36 +0000

    change unpublished posts route

--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   
   get '/projects', to: 'static_pages#projects'
+  get 'blog_posts/unpublished', to: 'blog_posts#unpublished'
   
   resources :blog_posts do
     patch :toggle_published, to: 'blog_posts#toggle', as: 'toggle_published'
   end
-  get '/all_blog_posts', to: 'blog_posts#all_blog_posts'
+  
   
   get '/admin_login', to: 'sessions#new'
   post '/admin_login', to: 'sessions#create'
