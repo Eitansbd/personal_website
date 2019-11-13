f15db03925a432830b5dae421b758ab340e542b9
Eitan Bar-David <eitansbd@gmail.com>
2019-11-13 15:00:36 +0000

    debbug paperclip config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -101,10 +101,10 @@ Rails.application.configure do
   storage: :s3,
   #:s3_host_name => 'REMOVE_THIS_LINE_IF_UNNECESSARY',
   s3_credentials: {
-    :access_key_id => AWS_ACCESS_KEY_ID,
-    :secret_access_key => AWS_SECRET_ACCESS_KEY,
-    :s3_region => "YOUR_S3_REGION_HERE"
+    :access_key_id =>  ENV.fetch('PERSONAL_WEBSITE_ACCESS_KEY_ID'),
+    :secret_access_key => ENV.fetch('PERSONAL_WEBSITE_SECRET_ACCESS_KEY'),
+    :s3_region => ENV.fetch('PERSONAL_WEBSITE_S3_REGION')
   },
-  bucket: 'S3_BUCKET_NAME'
+  bucket: ENV.fetch('PERSONAL_WEBSITE_BUCKET_NAME')
 }
 end
