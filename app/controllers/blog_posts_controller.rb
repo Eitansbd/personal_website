a829d8607e406b8f1bd25d5c27f87c3055d51c70
Eitan Bar-David <eitansbd@gmail.com>
2019-11-17 05:03:13 +0000

    change order to desc

--- app/controllers/blog_posts_controller.rb
+++ app/controllers/blog_posts_controller.rb
@@ -4,7 +4,7 @@ class BlogPostsController < ApplicationController
   before_action :require_admin_login, only: [:new, :create, :edit, :destroy, :unpublished, :toggle]
   
   def index
-    @blog_posts = BlogPost.where(published: true).order(:created_at)
+    @blog_posts = BlogPost.where(published: true).order(created_at: :desc)
   end
   
   def new
@@ -48,7 +48,7 @@ class BlogPostsController < ApplicationController
   end
   
   def unpublished
-    @blog_posts = BlogPost.where(published: false).order(:created_at)
+    @blog_posts = BlogPost.where(published: false).order(created_at: :desc)
     
   end
   
